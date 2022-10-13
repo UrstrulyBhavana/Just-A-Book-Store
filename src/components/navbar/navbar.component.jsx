4aae289077b29cbcb0c59a097e06fb7a913f0d17
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-10-13 17:06:48 -0400

    WIP product page

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 <div>
     <nav className="bg-white dark:bg-gray-800  ">
         <div className="max-w-7xl mx-auto px-8">
-            <div className="flex items-center justify-between h-16">
+            <div className="flex items-center justify-between h-20">
                 <div className=" flex items-center">
                     <div className="md:block">
                         <div className="flex items-baseline space-x-4">
--- src/routes/product-page/product-page.component.jsx
+++ src/routes/product-page/product-page.component.jsx
@@ -3,15 +3,34 @@ import { products } from "../../utils/products-data";
 
 const Product = () => {
     const { productId } = useParams();
-    const product = products.find(prod => prod.id === productId);
+    const product = products.find(prod => prod.id.toString() === productId);
+
     const { id, title, author, cover, price, description } = product;
 
     return (
-        <h1>
-            this is Product
-            {title}, {author}, {cover}, {price}, {description}
-            
-        </h1>
+        <div className="mt-10 flex justify-around">
+            <div className=" w-4/6 mx-20 overflow-hidden">
+                <h1 className="text-gray-400 pb-2 text-md font-medium"><span className="text-gray-800">Title:</span> {title}</h1>
+                <h2 className="text-gray-400 pb-2 text-md font-medium"><span className="text-gray-800">Author(s):</span> {author}</h2>
+                <h3 className="text-gray-400 text-md font-medium"><span className="text-gray-800">Description:</span> {description}</h3>
+            </div>
+            <div className="w-72 mx-20">
+                <div className="overflow-hidden">
+                    <img className="h-full w-full object-cover object-center lg:h-full lg:w-full rounded-lg" src={cover} alt={title} />
+                </div>
+                
+                <div className="my-10">
+                    <p>Price: ${price}</p>
+                    <p>Quantity: quantity</p>
+                </div>
+
+                <div>
+                    <button>Add to Cart</button>
+                </div>
+
+
+            </div>
+        </div>
     );
 }
 
